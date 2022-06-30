0c34822144adf37cda1ed7ba66e55b6cd46f8a76
Shem-aiah <SINelson@student.fullsail.edu>
2022-06-29 20:23:56 -0400

    Alternative MS

--- MSSE/cMain.cpp
+++ MSSE/cMain.cpp
@@ -1,1 +1,97 @@
 #include "cMain.h"
+
+wxBEGIN_EVENT_TABLE(cMain, wxFrame)
+wxEND_EVENT_TABLE()
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Mine Sweeper - SIN.version!", wxPoint(30, 30), wxSize(800, 600))
+{
+	wxMessageBox("Try to Hit the Bombs");
+	btn = new wxButton * [nFieldWidth * nFieldHeight];
+	wxGridSizer* grid = new wxGridSizer(nFieldWidth, nFieldHeight, 0, 0);
+	mineField = new int[nFieldWidth * nFieldHeight];
+	wxFont font(24, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false);
+	for (int x = 0; x < nFieldWidth; x++)
+	{
+		for (int y = 0; y < nFieldHeight; y++)
+		{
+			btn[y * nFieldWidth + x] = new wxButton(this, 10000 + (y * nFieldWidth + x));
+			btn[y * nFieldWidth + x]->SetFont(font);
+			grid->Add(btn[y * nFieldWidth + x], 1, wxEXPAND | wxALL);
+
+			btn[y * nFieldWidth + x]->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+			mineField[y * nFieldWidth + x] = 0;
+		}
+	}
+
+	this->SetSizer(grid);
+	grid->Layout();
+}
+
+
+cMain::~cMain()
+{
+	delete[] btn;
+}
+
+void cMain::OnButtonClicked(wxCommandEvent& evt)
+{
+	int xPos = (evt.GetId() - 10000) % nFieldWidth;
+	int yPos = (evt.GetId() - 10000) / nFieldWidth;
+
+	if (bFirstClick)
+	{
+		int mines = 30;
+		while (mines)
+		{
+			int randomXPos = rand() % nFieldWidth;
+			int randomYPos = rand() % nFieldHeight;
+
+			if (mineField[randomYPos * nFieldWidth + randomXPos] == 0 && randomXPos != xPos && randomYPos != yPos)
+			{
+				mineField[randomYPos * nFieldWidth + randomXPos] = -1;
+				mines--;
+			}
+		}
+
+		bFirstClick = false;
+	}
+
+	btn[yPos * nFieldWidth + xPos]->Enable(false);
+
+	if (mineField[yPos * nFieldWidth + xPos] == 0)
+	{
+		wxMessageBox("You Hit Nothing!!! - Try again");
+		bFirstClick = true;
+		for (int x = 0; x < nFieldWidth; x++)
+		{
+			for (int y = 0; y < nFieldHeight; y++)
+			{
+				mineField[y * nFieldWidth + x] = 0;
+				btn[y * nFieldWidth + x]->SetLabel("");
+				btn[y * nFieldWidth + x]->Enable(true);
+			}
+		}
+	}
+	else
+	{
+		int mines_left = 0;
+		for (int i = -1; i < 2; i++)
+		{
+			for (int j = -1; j < 2; j++)
+			{
+				if (xPos + i >= 0 && xPos + i < nFieldWidth && yPos + j >= 0 && yPos + j < nFieldHeight)
+				{
+					if (mineField[(yPos + j) * nFieldWidth + (xPos + i)] == -1)
+					{
+						mines_left++;
+					}
+				}
+			}
+		}
+		if (mines_left > 0)
+		{
+			btn[yPos * nFieldWidth + xPos]->SetLabel(std::to_string(mines_left));
+		}
+	}
+	evt.Skip();
+}
--- MSSE/cMain.h
+++ MSSE/cMain.h
@@ -1,10 +1,21 @@
 #pragma once
 #include "wx/wx.h"
 
-class cMain
+class cMain : public wxFrame
 {
 public:
 	cMain();
 	~cMain();
+
+public:
+	int nFieldWidth = 10;
+	int nFieldHeight = 10;
+	wxButton **btn;
+	int *mineField = nullptr;
+	bool bFirstClick = true;
+
+	void OnButtonClicked(wxCommandEvent &evt);
+
+	wxDECLARE_EVENT_TABLE();
 };
 
